34cbdb8960e66ba94f5b340ef0b9774658037702
Johan Sydseter <johan.sydseter@startsiden.no>
2013-01-11 16:56:52 +0100

    added puppet build manifest

---
+++ puppet/manifests/nodes/wheezybuild64.startsiden.no.pp
@@ -0,0 +1,7 @@
+# == node: vagrant-sydseter-debian-wheezy-amd64-puppet-ext
+# build environment for puppet development
+# === Authors
+# Johan Sydseter <johan.sydseter@startsiden.no>
+node 'wheezybuild64.startsiden.no' {
+
+}
--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -3,4 +3,5 @@
 # import "nodes/*.pp"
 
 # import nodes
+import "nodes/wheezybuild64.startsiden.no.pp"
 import "nodes/vagrant-sydseter-debian-wheezy-amd64-puppet-ext.vagrantup.com.pp"
